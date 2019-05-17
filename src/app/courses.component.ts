d4e7ff2fa34a9d3349070d3df94e78f812bf7ea8
mbandyopadhyay10 <mita.friendzforever@gmail.com>
2019-05-17 18:03:24 +0530

    1 file modified

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,10 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: `<h2>{{title}}</h2>`,
+  template: `<h2>{{title}}</h2>
+              <ul>
+                <li *ngFor="let course of courses">
+                    {{course}}
+                </li>
+              </ul>`,
   styles: [`h1 { font-family: sans-serif; }`],
 })
 export class CoursesComponent {
   title = 'My favourite Courses';
+  courses = ['Angular','SpringBoot','Spring'];
 }
 
