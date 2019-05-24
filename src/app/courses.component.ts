1c7a3069b065a18cea5c7e72b89a171447b051d0
mbandyopadhyay10 <mita.friendzforever@gmail.com>
2019-05-24 17:11:03 +0530

    2 files modified

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,15 +8,21 @@ import {CoursesService} from './courses.service'
                 <li *ngFor="let course of courses">
                     {{course}}
                 </li>
-              </ul>`,
+              </ul>
+              <input [(ngModel)]="email" (keyup.enter)="onEnterClick()">
+              `,
   styles: [`h1 { font-family: sans-serif; }`],
 })
 export class CoursesComponent {
   title = 'My favourite Courses';
   courses ;
+  email="m.bandyopadhyay@gmail.com";
 
   constructor(service :CoursesService){
       this.courses = service.getCourses();
+  }
+  onEnterClick(){
+    console.log(this.email);
   }
 }
 
